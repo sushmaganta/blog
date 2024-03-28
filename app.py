3bf92ba405f848e79a9c724a87704d8ef3df9996
sushmaganta <gantasushma8@gmail.com>
2024-03-28 10:51:50 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,103 @@
+from flask import Flask,render_template,request,redirect,url_for,session
+import mysql.connector
+app=Flask(__name__)
+app.secret_key="my secretkey is too secret"
+mydb=mysql.connector.connect(host="localhost",user="root",password="system",database="blogs")
+with mysql.connector.connect(host="localhost",user="root",password="system",database="blogs"):
+    cursor=mydb.cursor(buffered=True)
+    cursor.execute("create table if not exists login(username varchar(50) primary key,password varchar(50))") 
+@app.route("/")
+def home():
+    return render_template('homepage.html') 
+@app.route("/reg",methods=['GET','POST'])
+def register():
+    if request.method=="POST":
+        username=request.form['username']
+        mobile=request.form['mobile']
+        email=request.form['email']
+        address=request.form['address']
+        password=request.form['password']
+        cursor=mydb.cursor(buffered=True)
+        cursor.execute('insert into registration values(%s,%s,%s,%s,%s)',[username,mobile,email,address,password])
+        mydb.commit()
+        cursor.close()
+        return redirect(url_for('login'))
+    return render_template('register.html')
+@app.route("/login",methods=['GET','POST'])
+def login():
+    if request.method=="POST":
+        username=request.form['username']
+        password=request.form['password']
+        cursor=mydb.cursor(buffered=True)
+        cursor.execute('select count(*) from registration where username=%s && password=%s',[username,password])
+        data=cursor.fetchone()[0]
+        print(data)
+        if data==1:
+            session['username']=username
+            if not session.get(session['username']):
+                session[session['username']]={}
+            return redirect(url_for('home'))
+        else:
+            return "Invalid Username and Password"
+            
+    return render_template("login.html")
+@app.route("/logout",methods=['GET','POST'])
+def logout():
+    if session.get('username'):
+        session.pop('username')
+    return render_template(url_for('login'))
+@app.route('/admin',methods=['GET','POST'])
+def admin():
+    return render_template('admin.html')
+@app.route('/addposts',methods=['GET','POST'])
+def addposts():
+    if request.method=="POST":
+        title=request.form["title"]
+        content=request.form["content"]
+        slug=request.form["slug"]
+        print(title)
+        print(content)
+        print(slug)
+        cursor=mydb.cursor(buffered=True)
+        cursor.execute('Insert into posts(title,content,slug) values(%s,%s,%s)',(title,content,slug))
+        mydb.commit()
+        cursor.close()
+    return render_template('add_post.html')
+@app.route('/viewpost')
+def viewpost():
+        cursor=mydb.cursor(buffered=True)
+        cursor.execute('select * from posts')
+        posts=cursor.fetchall()
+        print(posts)
+        cursor.close()
+        return render_template('viewpost.html',posts=posts)
+@app.route('/delete_post/<int:id>',methods=['POST'])
+def delete_post(id):
+    cursor=mydb.cursor(buffered=True)
+    cursor.execute('select * from posts where id=%s',(id,))
+    post=cursor.fetchone()
+    cursor.execute('DELETE FROM posts WHERE id=%s',(id,))
+    mydb.commit()
+    cursor.close()
+    return redirect(url_for('viewpost'))
+@app.route('/update_post/<int:id>',methods=['GET','POST'])
+def update_post():
+    if request.method=="POST":
+        title=request.form["title"]
+        content=request.form["content"]
+        slug=request.form["slug"]
+        print(title)
+        print(content)
+        print(slug)
+        cursor=mydb.cursor(buffered=True)
+        cursor.execute('UPDATE posts SET title=%s,content=%s,slug=%s WHERE id=%s',(title,content,slug,id))
+        mydb.commit()
+        cursor.close()
+        return redirect(url_for('view_post'))
+    else:
+        cursor=mydb.cursor(buffered=True)
+        cursor.execute('select * from posts where id=%s',(id,))
+        post=cursor.fetchone()
+        cursor.close()
+        return render_template('update.html',post=post)
+app.run()
